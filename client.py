ddfaf426249eb3a09b0bb1db5b877bd2820092ac
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-02-15 17:47:51 +0200

    implement basic broadcast

--- client.py
+++ client.py
@@ -1,10 +1,34 @@
-import socket # Import socket module
+import socket 
+import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 host = socket.gethostname() # Get server host name
 port = 8080 
 
+uid = input("Choose a nickname: ")
+
 
 s.connect((host, port)) # Connect to server
-print(s.recv(1024).decode()) # Print message from server
-s.close() # Close the socket when done
+
+def receive():
+    while True:
+        try:
+            message = s.recv(1024).decode('ascii')
+            if message == 'NICK':
+                s.send(uid.encode('ascii'))
+            else:
+                print(message)
+        except:
+            s.close()
+            break
+
+def write():
+    while True:
+        message = input("")
+        s.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- server.py
+++ server.py
@@ -1,7 +1,25 @@
 import socket
+import threading
+from typing import List
 
 host = "::" # Listen on all available interfaces (both ipv4 and ipv6)
 port = 8080 
+
+class Client():
+    def __init__(self, uid: str, client_socket: socket):
+        self.uid: str = uid
+        self.socket: socket = client_socket
+        self.is_online: bool = True
+        self.buffered_messages: List[str] = [] 
+    
+class Group():
+    def __init__(self, uid: str):
+        self.uid: str = uid
+        self.members: List[Client] = [] 
+
+
+clients = {}
+groups = {}
 
 addr = (host, port) 
 if socket.has_dualstack_ipv6():
@@ -10,8 +28,36 @@ else:
     s = socket.create_server(addr)
 
 s.listen(5) # Now wait for client connection.
-while True:
-    c, addr = s.accept() # Establish connection with client.
-    print('Got connection from', addr)
-    c.send('Thank you for connecting'.encode())
-    c.close() # Close the connection
+
+
+def broadcast(message: str):
+    for v in clients.values():
+        v.socket.send(message)
+
+
+def handle(client: socket, uid: str):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except Exception as e:
+            print(e)
+            clients.get(uid).is_online = False
+            print("Client connection closed")
+            client.close()
+            break
+
+def receive():
+    print("Server started")
+    while True:
+        client,address = s.accept()
+        print(f"Connected with {str(address)}")
+        client.send('NICK'.encode('ascii'))
+        uid: str = client.recv(1024).decode('ascii')
+        clients.update({uid: Client(uid, client)})
+        client.send('Connected'.encode('ascii'))
+        
+        thread = threading.Thread(target=handle, args=(client, uid))
+        thread.start()
+
+receive()
